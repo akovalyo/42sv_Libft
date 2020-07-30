facf90239b3dc1a66efc89187b537ec430cce1de
Alexandr Kovalyov <al.kovalyov@gmail.com>
2020-07-30 12:36:41 -0700

    upd ft_atoi_base

--- srcs/ft_atoi_base.c
+++ srcs/ft_atoi_base.c
@@ -12,37 +12,53 @@
 
 #include "libft.h"
 
-static int	check_base(char c, int base)
+static int	convert_ch(char c, int base, int *nbr)
 {
-	int int_c;
+	int		c_int;
 
-	if (base <= 10)
-	{
-		int_c = ft_atoi(&c);
-		return (c >= '0' && int_c < base);
-	}
-	return ((c >= '0' && c <= '9') || (c >= 'A' && c <= ('A' + base - 10)) || \
-	(c >= 'a' && c <= ('a' + base - 10)));
-}
-
-static int	convert_ch(char c, int nbr, int base)
-{
-	if (c >= 'A' && c <= 'F')
-		return ((nbr * base) + (c - 'A' + 10));
+	if (c >= '0' && c <= '9')
+		c_int = c - '0';
+	else if (c >= 'A' && c <= 'F')
+		c_int = c - 'A' + 10;
 	else if (c >= 'a' && c <= 'f')
-		return ((nbr * base) + (c - 'a' + 10));
+		c_int = c - 'a' + 10;
 	else
-		return ((nbr * base) + (c - '0'));
+		return (0);
+	if (c_int < base)
+		*nbr = *nbr * base + c_int;
+	else
+		return (0);
+	return (1);
 }
 
-int			ft_atoi_base(char *str, char *base)
+static int	ft_atoi_cut(const char *nptr)
+{
+	int		collector;
+	int		i;
+
+	collector = 0;
+	i = 0;
+	while (nptr[i] == '\n' || nptr[i] == '\t' || nptr[i] == '\v' ||
+			nptr[i] == '\r' || nptr[i] == '\f' || nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+		return (0);
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		collector = collector * 10 + nptr[i] - '0';
+		i++;
+	}
+	return (collector);
+}
+
+int			ft_atoi_base_original(char *str, char *base)
 {
 	int	i;
 	int	sign;
 	int	nbr;
 	int	int_base;
 
-	int_base = ft_atoi(base);
+	int_base = ft_atoi_cut(base);
 	if (!str || (int_base < 2 || int_base > 16))
 		return (0);
 	i = 0;
@@ -56,10 +72,7 @@ int			ft_atoi_base(char *str, char *base)
 		sign = (str[i] == '-') ? -1 : 1;
 		i++;
 	}
-	while (str[i] && check_base(str[i], int_base))
-	{
-		nbr = convert_ch(str[i], nbr, int_base);
+	while (str[i] && convert_ch(str[i], int_base, &nbr))
 		i++;
-	}
 	return (nbr * sign);
 }
